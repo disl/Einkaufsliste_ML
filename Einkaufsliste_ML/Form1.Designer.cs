16ccd35207bacb5e8705766c3c120ed9c47b81b6
Dimitri Sluzki <dimitri.sluzki@haas.de>
2025-05-14 15:32:46 +0200

    14.05.25  15:32

--- Einkaufsliste_ML/Form1.Designer.cs
+++ Einkaufsliste_ML/Form1.Designer.cs
@@ -28,10 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             label1 = new Label();
             textBox1 = new TextBox();
             dataGridView1 = new DataGridView();
+            articleDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            categoryDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            valueDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            PredictionTypeBindingSource = new BindingSource(components);
+            dataSet1 = new DataSet1();
+            category_saveButton = new Button();
+            articleTextBox = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            categorieTextBox = new TextBox();
+            category_deleteButton = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PredictionTypeBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataSet1).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -47,23 +61,117 @@
             // 
             textBox1.Location = new Point(94, 42);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(495, 23);
+            textBox1.Size = new Size(381, 23);
             textBox1.TabIndex = 1;
+            textBox1.Text = "brot;apfel;bla";
             textBox1.KeyDown += textBox1_KeyDown;
             // 
             // dataGridView1
             // 
+            dataGridView1.AutoGenerateColumns = false;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { articleDataGridViewTextBoxColumn, categoryDataGridViewTextBoxColumn, valueDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = PredictionTypeBindingSource;
             dataGridView1.Location = new Point(94, 110);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(495, 328);
+            dataGridView1.Size = new Size(381, 328);
             dataGridView1.TabIndex = 2;
+            // 
+            // articleDataGridViewTextBoxColumn
+            // 
+            articleDataGridViewTextBoxColumn.DataPropertyName = "Article";
+            articleDataGridViewTextBoxColumn.HeaderText = "Article";
+            articleDataGridViewTextBoxColumn.Name = "articleDataGridViewTextBoxColumn";
+            // 
+            // categoryDataGridViewTextBoxColumn
+            // 
+            categoryDataGridViewTextBoxColumn.DataPropertyName = "Category";
+            categoryDataGridViewTextBoxColumn.HeaderText = "Category";
+            categoryDataGridViewTextBoxColumn.Name = "categoryDataGridViewTextBoxColumn";
+            // 
+            // valueDataGridViewTextBoxColumn
+            // 
+            valueDataGridViewTextBoxColumn.DataPropertyName = "Value";
+            valueDataGridViewTextBoxColumn.HeaderText = "Value";
+            valueDataGridViewTextBoxColumn.Name = "valueDataGridViewTextBoxColumn";
+            // 
+            // PredictionTypeBindingSource
+            // 
+            PredictionTypeBindingSource.DataMember = "PredictionType";
+            PredictionTypeBindingSource.DataSource = dataSet1;
+            // 
+            // dataSet1
+            // 
+            dataSet1.DataSetName = "DataSet1";
+            dataSet1.Namespace = "http://tempuri.org/DataSet1.xsd";
+            dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // category_saveButton
+            // 
+            category_saveButton.Location = new Point(481, 227);
+            category_saveButton.Name = "category_saveButton";
+            category_saveButton.Size = new Size(128, 23);
+            category_saveButton.TabIndex = 3;
+            category_saveButton.Text = "Kategorie speichern";
+            category_saveButton.UseVisualStyleBackColor = true;
+            category_saveButton.Click += category_saveButton_Click;
+            // 
+            // articleTextBox
+            // 
+            articleTextBox.DataBindings.Add(new Binding("Text", PredictionTypeBindingSource, "Article", true));
+            articleTextBox.Location = new Point(481, 128);
+            articleTextBox.Name = "articleTextBox";
+            articleTextBox.ReadOnly = true;
+            articleTextBox.Size = new Size(307, 23);
+            articleTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(481, 109);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Article";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(481, 159);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Categorie";
+            // 
+            // categorieTextBox
+            // 
+            categorieTextBox.DataBindings.Add(new Binding("Text", PredictionTypeBindingSource, "Category", true));
+            categorieTextBox.Location = new Point(481, 178);
+            categorieTextBox.Name = "categorieTextBox";
+            categorieTextBox.Size = new Size(307, 23);
+            categorieTextBox.TabIndex = 6;
+            // 
+            // category_deleteButton
+            // 
+            category_deleteButton.Location = new Point(660, 227);
+            category_deleteButton.Name = "category_deleteButton";
+            category_deleteButton.Size = new Size(128, 23);
+            category_deleteButton.TabIndex = 8;
+            category_deleteButton.Text = "Kategorie löschen";
+            category_deleteButton.UseVisualStyleBackColor = true;
+            category_deleteButton.Click += category_deleteButton_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(category_deleteButton);
+            Controls.Add(label3);
+            Controls.Add(categorieTextBox);
+            Controls.Add(label2);
+            Controls.Add(articleTextBox);
+            Controls.Add(category_saveButton);
             Controls.Add(dataGridView1);
             Controls.Add(textBox1);
             Controls.Add(label1);
@@ -71,6 +179,8 @@
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PredictionTypeBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataSet1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -80,5 +190,16 @@
         private Label label1;
         private TextBox textBox1;
         private DataGridView dataGridView1;
+        private Button category_saveButton;
+        private TextBox articleTextBox;
+        private Label label2;
+        private Label label3;
+        private TextBox categorieTextBox;
+        private BindingSource PredictionTypeBindingSource;
+        private DataSet1 dataSet1;
+        private DataGridViewTextBoxColumn articleDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn categoryDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn valueDataGridViewTextBoxColumn;
+        private Button category_deleteButton;
     }
 }
--- Einkaufsliste_ML/Form1.cs
+++ Einkaufsliste_ML/Form1.cs
@@ -1,47 +1,103 @@
+using Akavache;
+using System.Reactive.Linq;
+using System.Reflection;
+
 namespace Einkaufsliste_ML
 {
     public partial class Form1 : Form
     {
+        private IEnumerable<string> m_categories_in_cache;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void Form1_Load(object sender, EventArgs e)
         {
+            string appName = Assembly.GetEntryAssembly()?.GetName().Name ?? "UnknownApp";
+            BlobCache.ApplicationName = appName;
+
+            m_categories_in_cache = await BlobCache.LocalMachine.GetAllKeys();
+        }
+
+        private async void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                GetData();
+            }
+        }
+
+        private void GetData()
+        {
+            string[] strings = textBox1.Text.Split(new char[] { ';' });
+
+            if (strings.Length > 0)
+            {
+                dataSet1.PredictionType.Rows.Clear();
+
+                foreach (string s in strings)
+                {
+                    MLModel1.ModelInput sampleData = new MLModel1.ModelInput()
+                    {
+                        Col0 = s,
+                    };
+                    var sortedScoresWithLabel = MLModel1.PredictAllLabels(sampleData);
+                    if (sortedScoresWithLabel != null && sortedScoresWithLabel.Count() > 0)
+                    {
+                        var predicted_item = sortedScoresWithLabel.FirstOrDefault();
+
+                        dataSet1.PredictionType.AddPredictionTypeRow(s, predicted_item.Key, Convert.ToDecimal(predicted_item.Value));
+                    }
+                }
+                ApplyLocalTypes();
+            }
+        }
+
+        private async void ApplyLocalTypes()
+        {
+            foreach (var item in dataSet1.PredictionType)
+            {
+                if (m_categories_in_cache.Contains(item.Article))
+                {
+                    var local_category = await BlobCache.LocalMachine.GetObject<string?>(item.Article);
+                    if (local_category != null)
+                    {
+                        var row_searched = dataSet1.PredictionType.FindByArticle(item.Article);
+                        if (row_searched != null)
+                        {
+                            var ind = dataSet1.PredictionType.Rows.IndexOf(row_searched);
+                            if (ind != -1)
+                            {
+                                dataSet1.PredictionType[ind].Category = local_category;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private async void category_saveButton_Click(object sender, EventArgs e)
+        {
+            /*
+                // Objekt speichern
+                await BlobCache.LocalMachine.InsertObject("Artikel-123", "Elektronik", TimeSpan.FromDays(1));
+
+                // Objekt abrufen
+                string kategorie = await BlobCache.LocalMachine.GetObject<string>("Artikel-123");
+             */
+            await BlobCache.LocalMachine.InsertObject(articleTextBox.Text, categorieTextBox.Text, TimeSpan.FromDays(1));
+            m_categories_in_cache = await BlobCache.LocalMachine.GetAllKeys();
+            GetData();
 
         }
 
-        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        private async void category_deleteButton_Click(object sender, EventArgs e)
         {
-            List<KeyValuePair<string,float>> list_output = new ();
-
-            if (e.KeyCode == Keys.Enter)
-            {
-                string[] strings = textBox1.Text.Split(new char[] { ';' });
-
-                if (strings.Length > 0)
-                {
-                    dataGridView1.DataSource=null;
-
-                    foreach (string s in strings)
-                    {
-                        MLModel1.ModelInput sampleData = new MLModel1.ModelInput()
-                        {
-                            Col0 = s,
-                        };
-                        var sortedScoresWithLabel = MLModel1.PredictAllLabels(sampleData);
-                        if(sortedScoresWithLabel != null && sortedScoresWithLabel.Count() > 0)
-                        {
-                            var predicted_item = sortedScoresWithLabel.FirstOrDefault();
-
-                            list_output.Add(predicted_item);
-                        }
-                    }
-
-                    dataGridView1.DataSource = list_output;
-                }
-            }
+            await BlobCache.LocalMachine.Invalidate(articleTextBox.Text);
+            m_categories_in_cache = await BlobCache.LocalMachine.GetAllKeys();
+            GetData();
         }
     }
 
